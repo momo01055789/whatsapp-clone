70f5f8bb79a0faf30593cfa22cf6f4b42a89c768
momo01055789 <mohamed.elhadidy666@gmail.com>
2024-05-06 12:55:08 +0300

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: "greedy-ibis-684.convex.cloud" }],
+  },
+};
 
 export default nextConfig;
